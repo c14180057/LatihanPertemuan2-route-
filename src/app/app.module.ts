f3855b0c476225fd46bb3ae91161727be3db2a98
c1480057 <c14180057@john.petra.ac.id>
2020-04-22 09:32:49 +0700

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { InputComponent } from './input/input.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, InputComponent, EditComponent, ListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
