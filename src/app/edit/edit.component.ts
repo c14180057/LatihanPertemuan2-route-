78306f6db71e9fed8f6afb394da1f365fdfa56aa
c1480057 <c14180057@john.petra.ac.id>
2020-04-22 11:25:00 +0700

    2 files modified

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -9,16 +9,17 @@ export class EditComponent implements OnInit {
 
   constructor( private appComponent: AppComponent) { }
   list=[];
-  tomy=""
+  data;
   ngOnInit() {
     this.list=this.appComponent.getData();
     for(var i=0;i<this.list.length;i++)
     {
      if(this.list[i].values()=="Infor")
      {
-       this.tomy="Oke";
+       
      }
     }
+   this.data=this.list[0];
   }
 
 }
